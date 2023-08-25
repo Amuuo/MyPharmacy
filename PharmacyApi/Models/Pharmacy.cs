a006b40eb9b3aab34559c75220f11a5a7b328195
Adam <adamthewilliams@gmail.com>
2023-08-24 23:45:02 -0500

    modified pharmacy model

--- PharmacyApi/Models/Pharmacy.cs
+++ PharmacyApi/Models/Pharmacy.cs
@@ -6,15 +6,15 @@ namespace PharmacyApi.Models;
 [Table("pharmacy")]
 public class Pharmacy
 {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     
-    [StringLength(50)]  public string? Name    { get; set; }
+    [StringLength(50)]  public string Name    { get; set; }
     [StringLength(100)] public string? Address { get; set; }
     [StringLength(50)]  public string? City    { get; set; }
     [StringLength(2)]   public string? State   { get; set; }
     [StringLength(20)]  public string? Zip     { get; set; }
     
     public int? PrescriptionsFilled { get; set; }
-    public DateTime? CreatedDate { get; set; }
+    public DateTime CreatedDate { get; set; }
     public DateTime? UpdatedDate { get; set; }
 }

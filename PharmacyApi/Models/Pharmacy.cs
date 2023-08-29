740d2f074004e8c7a060881645fee6a4174fa5a1
Adam <adamthewilliams@gmail.com>
2023-08-29 13:52:04 -0500

    updated dbContext with new tables

--- PharmacyApi/Models/Pharmacy.cs
+++ PharmacyApi/Models/Pharmacy.cs
@@ -1,20 +1,27 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
 
 namespace PharmacyApi.Models;
 
-[Table("pharmacy")]
-public class Pharmacy
+public partial class Pharmacy
 {
     public int Id { get; set; }
-    
-    [StringLength(50)]  public string? Name    { get; set; }
-    [StringLength(100)] public string? Address { get; set; }
-    [StringLength(50)]  public string? City    { get; set; }
-    [StringLength(2)]   public string? State   { get; set; }
-    [StringLength(20)]  public string? Zip     { get; set; }
-    
+
+    public string Name { get; set; } = null!;
+
+    public string? Address { get; set; }
+
+    public string? City { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Zip { get; set; }
+
     public int? PrescriptionsFilled { get; set; }
+
     public DateTime CreatedDate { get; set; }
+
     public DateTime? UpdatedDate { get; set; }
+
+    public virtual ICollection<Pharmacist> Pharmacists { get; set; } = new List<Pharmacist>();
 }
---
+++ PharmacyApi/Models/Warehouse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PharmacyApi.Models;
+
+public partial class Warehouse
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string City { get; set; } = null!;
+
+    public string State { get; set; } = null!;
+
+    public string Zip { get; set; } = null!;
+}

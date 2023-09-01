2ac34ee7c17e292c9aa4cd921e5d2e235f68e50a
Adam <adamthewilliams@gmail.com>
2023-09-01 12:54:07 -0500

    included PharmacistList component and corresponding Slice/Service

--- webui/src/store.ts
+++ webui/src/store.ts
@@ -1,10 +1,19 @@
+import { pharmacistSlice } from './slices/pharmacistSlice';
 import { pharmacySlice } from './slices/pharmacySlice';
 import { configureStore } from '@reduxjs/toolkit';
-
+import { TypedUseSelectorHook, useSelector as untypedUseSelector, useDispatch } from 'react-redux';
 
 const store = configureStore({
-    reducer: pharmacySlice.reducer    
+    reducer: {
+        pharmacy: pharmacySlice.reducer,
+        pharmacist: pharmacistSlice.reducer
+    }
 });
 
+export type AppDispatch = typeof store.dispatch;
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export const useSelector: TypedUseSelectorHook<RootState> = untypedUseSelector;
 
 export default store;

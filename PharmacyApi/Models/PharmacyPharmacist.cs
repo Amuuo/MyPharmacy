5bb0bd0d4fa712ac78292ea206761358957b5808
Adam <adamthewilliams@gmail.com>
2023-09-02 15:27:53 -0500

    added PharmacyPharmacist entity

---
+++ PharmacyApi/Models/PharmacyPharmacist.cs
@@ -0,0 +1,10 @@
+﻿namespace PharmacyApi.Models;
+
+public class PharmacyPharmacist
+{
+    public int PharmacistId { get; set; }
+    public Pharmacist Pharmacist { get; set; } = null!;
+
+    public int PharmacyId { get; set; }
+    public Pharmacy Pharmacy { get; set; } = null!;
+}

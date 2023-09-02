7b7dc5c15065baefabdd2d556794bc6ea50f92d8
Adam <adamthewilliams@gmail.com>
2023-09-02 15:28:49 -0500

    added things for paged search results

---
+++ PharmacyApi/Utilities/Interfaces/IPagedSearchCriteria.cs
@@ -0,0 +1,11 @@
+﻿namespace PharmacyApi.Utilities.Interfaces
+{
+    public interface IPagedSearchCriteria<T>
+    {
+        int     PageNumber     { get; set; }
+        int     PageSize       { get; set; }
+        string? SortColumn     { get; set; }
+        string? SortDirection  { get; set; }
+        T       SearchCriteria { get; set; }
+    }
+}

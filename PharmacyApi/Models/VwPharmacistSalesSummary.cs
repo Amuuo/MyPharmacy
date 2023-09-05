32fcf2df28f225eb9b226e05a26527bd8afba8a0
Adam <adamthewilliams@gmail.com>
2023-09-05 15:27:06 -0500

    updated pharmacistSaleSummary model

--- PharmacyApi/Models/VwPharmacistSalesSummary.cs
+++ PharmacyApi/Models/VwPharmacistSalesSummary.cs
@@ -2,11 +2,13 @@
 
 public partial class VwPharmacistSalesSummary
 {
+    public int PharmacistId { get; set; }
+
     public string FirstName { get; set; } = null!;
 
     public string LastName { get; set; } = null!;
 
-    public string Pharmacy { get; set; } = null!;
+    public string PharmacyList { get; set; } = null!;
 
     public string PrimaryRx { get; set; } = null!;
 

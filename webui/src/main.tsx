c240a98525e59d435055e22a927103d3326c3749
Adam <adamthewilliams@gmail.com>
2023-09-07 14:44:12 -0500

    added routing to main.tsx

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import store from './store/store.ts';
 import PharmacyManager  from './pages/PharmacyManager/PharmacyManager.tsx';
-import './index.scss'
-import { Provider } from 'react-redux'
+import './index.scss';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.tsx';
+import ReportManager from './pages/ReportManager/ReportManager.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -12,7 +14,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <div className="main-container">
         <Header/>
-        <PharmacyManager/>
+        <div style={{padding: '3rem'}}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<PharmacyManager/>}/>
+              <Route path="reports" element={<ReportManager/>}/>            
+            </Routes>
+          </BrowserRouter>        
+        </div>
       </div>
     </Provider>
   </React.StrictMode>,

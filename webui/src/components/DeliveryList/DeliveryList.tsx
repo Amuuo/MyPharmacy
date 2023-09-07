03b221887cc4f0897e0d29ab6ec417573c499067
Adam <adamthewilliams@gmail.com>
2023-09-07 14:40:11 -0500

    add/refactor delivery list

--- webui/src/components/DeliveryList/DeliveryList.tsx
+++ webui/src/components/DeliveryList/DeliveryList.tsx
@@ -1,61 +1,60 @@
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useSelector } from "../../store/store";
+import { AppDispatch, useSelector } from "../../store/store";
 import { getDeliveryListByPharmacyId } from "../../services/deliveryService";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import './DeliveryList.scss';
 import { LinearProgress } from "@mui/material";
 
-const DeliveryList: React.FC = () => {
+export default function DeliveryList() {
     
-    const dispatch = useDispatch();
-    const selectedPharmacy = useSelector(state => state.pharmacy.selectedPharmacy);
-    const deliveryList = useSelector(state => state.delivery.deliveryList);
-    const loading = useSelector(state => state.delivery.loading);
+    const dispatch: AppDispatch = useDispatch();
+    const { selectedPharmacy, deliveryList, loading } = useSelector(state => ({
+        selectedPharmacy: state.pharmacy.selectedPharmacy,
+        deliveryList: state.delivery.deliveryList,
+        loading: state.delivery.loading
+    }));
 
     useEffect(() => {   
         if (selectedPharmacy.id)
-            dispatch(getDeliveryListByPharmacyId(selectedPharmacy.id) as any);
+            dispatch(getDeliveryListByPharmacyId(selectedPharmacy.id));
 
     }, [selectedPharmacy]);
     
-    function formatCurrency(value: number, currency: string = 'USD', locale: string = 'en-US'): string {
-        return new Intl.NumberFormat(locale, { style: 'currency', currency }).format(value);
-    }
+    const formatCurrency = (value: number) => 
+        new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);    
 
-    const columns: GridColDef[] = useMemo(() => ([                    
-            { field: 'drugName', headerName: 'Drug Name', width: 120, editable: true },
-            { field: 'unitCount', headerName: 'Count', width: 60, editable: true, type: 'number'},
-            { field: 'unitPrice', headerName: 'Price', width: 80, editable: true,  type: 'number', valueFormatter: (params) => formatCurrency(params.value) },
-            { field: 'totalPrice', headerName: 'Total', width: 100, editable: true,  type: 'number', valueFormatter: (params) => formatCurrency(params.value) },
-            { field: 'deliveryDate', headerName: 'Delivery Date', width: 180, editable: true }        
-    ]), []);
-
-
-    if (!selectedPharmacy?.id) {        
-        return null;
-    }
-       
-    if (loading) {
-        return <LinearProgress className="delivery-list"/>;
-    }
-    
-    if (!loading && (deliveryList.length === 0)) {
-        return <h3 style={{textAlign: 'center'}} className="delivery-list">No deliveries found...</h3>;
-    }
+    const formatDate = (date: Date) => 
+        date.toLocaleString('en-US', {
+            day: '2-digit', 
+            month: '2-digit', 
+            year: '2-digit', 
+            hour: 'numeric', 
+            minute: 'numeric', 
+            hour12: false}).replace(/\//g, '-');
+                          
+    const columns: GridColDef[] = [                    
+            { field: 'drugName', headerName: 'Drug Name', width: 150 },
+            { field: 'unitCount', headerName: 'Count', width: 60, type: 'number'},
+            { field: 'unitPrice', headerName: 'Price', width: 80,  type: 'number', valueFormatter: (params) => formatCurrency(params.value) },
+            { field: 'totalPrice', headerName: 'Total', width: 120,  type: 'number', valueFormatter: (params) => formatCurrency(params.value) },
+            { field: 'deliveryDate', headerName: 'Delivery Date', width: 150, valueFormatter: (params) => formatDate(new Date(params.value))}
+    ];
 
     return (
-        <div className="delivery-list">            
-            <DataGrid             
-                columns={columns}
-                rows={deliveryList}
-                rowHeight={30}
-                rowCount={deliveryList.length}
-                hideFooter={true}
-                columnHeaderHeight={35}/>                
-        </div>
-    );
-
+        !selectedPharmacy?.id 
+            ? null 
+            : loading 
+                ? <LinearProgress/> 
+                : (deliveryList.length === 0) 
+                    ? <h3 style={{textAlign: 'center'}} className="delivery-list">No deliveries found...</h3> 
+                    : <div className="delivery-list">            
+                        <DataGrid
+                            columns={columns}
+                            rows={deliveryList}
+                            rowHeight={30}                
+                            hideFooter={true}
+                            columnHeaderHeight={35}/>                
+                      </div>        
+    );    
 }
-
-export default DeliveryList;

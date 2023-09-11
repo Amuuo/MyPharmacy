c5eb5048b91a43deb122880bf549293550bb1069
Adam <adamthewilliams@gmail.com>
2023-09-11 14:01:58 -0500

    formatting/ocd

--- PharmacyApi/Data/IPharmacyDbContext.cs
+++ PharmacyApi/Data/IPharmacyDbContext.cs
@@ -5,14 +5,14 @@ namespace PharmacyApi.Data
 {
     public interface IPharmacyDbContext : IDisposable
     {
-        DbSet<Pharmacy>   PharmacyList   { get; }
-        DbSet<Delivery>   DeliveryList   { get; }
-        DbSet<Pharmacist> PharmacistList { get; }
-        DbSet<Warehouse>  WarehouseList  { get; }
-        DbSet<PharmacyPharmacist> PharmacyPharmacists { get; }
-        DbSet<VwDeliveryDetail> VwDeliveryDetails { get; set; }
-        DbSet<VwPharmacistSalesSummary> VwPharmacistSalesSummaries { get; set; }
-        DbSet<VwWarehouseProfit> VwWarehouseProfits { get; set; }
+        DbSet<Pharmacy>                 PharmacyList               { get; }
+        DbSet<Delivery>                 DeliveryList               { get; }
+        DbSet<Pharmacist>               PharmacistList             { get; }
+        DbSet<Warehouse>                WarehouseList              { get; }
+        DbSet<PharmacyPharmacist>       PharmacyPharmacists        { get; }
+        DbSet<VwDeliveryDetail>         VwDeliveryDetails          { get; }
+        DbSet<VwPharmacistSalesSummary> VwPharmacistSalesSummaries { get; }
+        DbSet<VwWarehouseProfit>        VwWarehouseProfits         { get; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }

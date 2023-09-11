cea9c91d2da3a2ce25f4cdfe0d91e29ca519073c
Adam <adamthewilliams@gmail.com>
2023-09-11 14:01:43 -0500

    modified warehouse profit view to group on id

--- PharmacyApi/Data/Scripts/CreateWarehouseProfitView.sql
+++ PharmacyApi/Data/Scripts/CreateWarehouseProfitView.sql
@@ -1,12 +1,23 @@
 CREATE VIEW vw_warehouse_profit AS
+WITH warehouse_revenues AS (
+    SELECT     
+        w.id               warehouse_id,
+        SUM(d.total_price) total_delivery_revenue,
+        SUM(d.unit_count)  total_unit_count,
+        SUM(d.total_price) / SUM(d.unit_count) average_profit_per_unit
+    FROM 
+        delivery d
+    JOIN 
+        warehouse w ON d.warehouse_id = w.id
+    GROUP BY 
+        w.id
+)
+
 SELECT 
-    w.name             warehouse,
-    SUM(d.total_price) total_delivery_revenue,
-    SUM(d.unit_count)  total_unit_count,
-    SUM(d.total_price) / SUM(d.unit_count) average_profit_per_unit
+    w.name warehouse,
+    wr.total_delivery_revenue,
+    wr.total_unit_count,
+    wr.average_profit_per_unit
 FROM 
-    delivery d
-JOIN 
-    warehouse w ON d.warehouse_id = w.id
-GROUP BY 
-    w.name
+    warehouse_revenues wr
+JOIN warehouse w ON wr.warehouse_id = w.id
